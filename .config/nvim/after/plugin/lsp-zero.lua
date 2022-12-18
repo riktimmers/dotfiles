7f8be5652424ea0c5bf17fd5630f90b562fcbcbc
Rik <riktimmers@hotmail.com>
2022-12-18 12:50:37 +0100

    Added nvim settings

---
+++ .config/nvim/after/plugin/lsp-zero.lua
@@ -0,0 +1,32 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+local cmp = require('cmp')
+local cmp_select = {behaviour = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<CR>'] = cmp.mapping.confirm({ select = true }),
+	['<C-space>'] = cmp.mapping.complete(), 
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+	print("help")
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.hover() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.nvim_workspace()
+lsp.setup()
+
---
+++ .config/nvim/lua/general_setup/init.lua
@@ -0,0 +1,3 @@
+require("general_setup.remap")
+require("general_setup.packer")
+require("general_setup.set")
