21f61a9e6c0b288bfda047667d34850a31915455
Rik <rik@desktop>
2022-12-19 19:49:51 +0100

    Update to neovim

--- install.sh
+++ install.sh
@@ -13,10 +13,6 @@ sudo apt install -y tmux tmuxinator
 # Install i3
 sudo apt install -y i3 i3status
 
-# Install kitty 
-sudo apt install -y curl
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 
 
 
